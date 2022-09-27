b0c900f1dc01c3b8ba91a67eb035ecd339a49ca1
Carlos Gauto <charlyautomatiza@gmail.com>
2022-09-26 23:31:22 -0300

    test: data management examples

---
+++ tests/dataClassExample.spec.ts
@@ -0,0 +1,58 @@
+import { test, expect, APIRequestContext } from '@playwright/test';
+import { Login } from './pageobjects/login';
+import { UserData } from '../utils/userData';
+import * as users from '../data/users.json';
+
+// Request context is reused by all tests in the file.
+let apiContext: APIRequestContext;
+const userData = new UserData();
+
+test.beforeAll(async ({ playwright }) => {
+  apiContext = await playwright.request.newContext({
+    // All requests we send go to this API endpoint.
+    baseURL: 'https://task-mgmt-charlyautomatiza.herokuapp.com',
+    extraHTTPHeaders: {
+      Accept: 'application/json',
+    },
+  });
+});
+
+test.afterAll(async () => {
+  // Dispose all responses.
+  await apiContext.dispose();
+});
+
+/**
+ * This test is a simple smoke test.
+*/
+test('API SignUp | Login UI', async ({ page }) => {
+  // New User
+  const username = userData.getUsername();
+  const password = userData.getPassword();
+  const newUser = await apiContext.post('/auth/signup', {
+    data: {
+      username,
+      password,
+    },
+  });
+  expect(newUser.ok()).toBeTruthy();
+
+  const login = new Login(page);
+  await login.goto();
+  await login.sigIn(username, password);
+  await page.waitForURL('**/tasks');
+});
+
+/**
+ * This test is a simple smoke test.
+*/
+test('Login UI - Data From JSON', async ({ page }) => {
+  // New User
+  const { username } = users[0];
+  const { password } = users[0];
+
+  const login = new Login(page);
+  await login.goto();
+  await login.sigIn(username, password);
+  await page.waitForURL('**/tasks');
+});
